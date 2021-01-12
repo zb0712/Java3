5ffff467f2fd4444d6ad2df534642766252b187c
zb0712 <74041261+zb0712@users.noreply.github.com>
2021-01-12 21:13:25 +0800

    Create 建表sql1.sql

---
+++ Java3/建表sql1.sql
@@ -0,0 +1,15 @@
+CREATE TABLE country(
+confirmed INT,
+recovered INT,
+deaths INT,
+country VARCHAR(50) PRIMARY KEY NOT NULL,
+population INT,
+sq_km_area DOUBLE,
+life_expectancy VARCHAR(50),
+elevation_in_meters VARCHAR(50),
+continent VARCHAR(50),
+abbreviation VARCHAR(50),
+location VARCHAR(50),
+iso INT,
+capital_city VARCHAR(50)
+)
